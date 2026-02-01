09f5ac2dced802ae516dfbd82cc60a1c72be8767
shinjipark22 <shinjipark22@gmail.com>
2026-02-01 16:31:14 +0900

    Finalize DAG

---
+++ dags/politics_news_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+from news_crawler import crawl_naver_politics_by_date
+
+# 기본 설정
+default_args = {
+    'owner': 'shinji',
+    'depends_on_past': False,
+    'start_date' : datetime(2026, 1, 26), # 수집 시작 날짜
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5) # 재시도 텀
+}
+
+# DAG 정의
+with DAG(
+    'politics_news_daily', # DAG ID
+    default_args=default_args,
+    description='매일 네이버 정치 뉴스를 수집하는 파이프라인',
+    schedule_interval='@daily',
+    catchup=True,
+    tags=['politics', 'naver', 'project']
+) as dag:
+
+    crawling_task = PythonOperator(
+        task_id='daily_crawl_task',
+
+        # 실행할 파이썬 함수
+        python_callable=crawl_naver_politics_by_date,
+
+        # 날짜 전달
+        op_kwargs={'target_date': '{{ ds_nodash }}'},
+    )
+
+    # 작업 순서
+    crawling_task
